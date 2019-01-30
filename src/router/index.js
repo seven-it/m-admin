6f9facddb6a431caf46f5ad61c4f83aa8ebf66c2
seven-it <itchenxuanbin@sina.com>
2019-01-30 16:44:23 +0800

    调整路由信息，添加规则注释

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Layout from "@/views/layout";
+import { sessionStorage } from "@/utils/utils";
 
 Vue.use(Router);
+
+/**
+ * 自定义路由规则
+ * 
+ * hidden: Boolean              该路由是否显示在侧边导航栏
+ * meta: {
+    title: "title"              在侧边导航栏与面包屑中显示的路由名称
+    icon : "icon"               在侧边栏中显示的icon图标
+    roles: ["admin","editor"]   路由权限标识
+   }
+ */
 
 // 非权限路由组
 export const constantRouterMap = [
@@ -21,57 +33,7 @@ export const constantRouterMap = [
   {
     path: "/login",
     component: () => import("@/views/login/login"),
-    meta: { title: "登录", icon: "el-icon-setting" }
-  },
-  {
-    path: "/r1",
-    component: Layout,
-    meta: { title: "导航一", icon: "el-icon-setting" },
-    children: [
-      {
-        path: "/r1/welcome1",
-        meta: { title: "导航1-1", icon: "el-icon-setting" },
-        children: [
-          {
-            path: "/r1/welcome1/welcome1-1",
-            component: () => import("@/views/welcome/welcome"),
-            meta: { title: "导航1-1-1", icon: "el-icon-setting" }
-          },
-          {
-            path: "/r1/welcome1/welcome1-2",
-            component: () => import("@/views/welcome/welcome"),
-            meta: { title: "导航1-2-1", icon: "el-icon-setting" }
-          }
-        ]
-      },
-      {
-        path: "welcome2",
-        component: () => import("@/views/welcome/welcome"),
-        meta: { title: "导航1-2", icon: "el-icon-setting" }
-      }
-    ]
-  },
-  {
-    path: "/r2",
-    component: Layout,
-    children: [
-      {
-        path: "welcome3",
-        component: () => import("@/views/welcome/welcome"),
-        meta: { title: "导航二", icon: "el-icon-setting" }
-      }
-    ]
-  },
-  {
-    path: "/r3",
-    component: Layout,
-    children: [
-      {
-        path: "welcome4",
-        component: () => import("@/views/welcome/welcome"),
-        meta: { title: "导航三", icon: "el-icon-setting" }
-      }
-    ]
+    hidden: true
   }
 ];
 
@@ -102,13 +64,34 @@ export const asyncRouterMap = [
         meta: {
           title: "导航六",
           icon: "el-icon-setting",
-          role: ["super"]
+          role: ["admin", "super"]
         }
       }
     ]
   }
 ];
 
-export default new Router({
+const router = new Router({
   routes: constantRouterMap
 });
+
+// 拦截未登录用户并跳转到登陆页面
+router.beforeEach((to, from, next) => {
+  const token = sessionStorage.getItem("token");
+
+  /**
+   * tip: 关于beforeEach无限循环的问题
+   * url: https://segmentfault.com/a/1190000011042794
+   */
+  if (to.path === "/login") {
+    next();
+  } else {
+    if (!token) {
+      next("/login");
+    }
+  }
+
+  next();
+});
+
+export default router;
