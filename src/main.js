8f85c490eb21070646b87208e08797b1dbf923a5
seven-it <itchenxuanbin@sina.com>
2019-01-31 17:31:12 +0800

    引入自定义icon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
+
 import "@/styles/normalize.css"; // CSS resets
 import "@/styles/global.scss"; // global style
+import "./assets/font-icon/iconfont.css"; // custom icons
+
 import App from "./App.vue";
 import router from "./router/index";
 import store from "./store/index";
+
 import "./plugins/element.js";
 import api from "@/api";
 
