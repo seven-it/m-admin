2adbfcb3c7abd4d3868497244ebea6389146b587
seven-it <itchenxuanbin@sina.com>
2019-02-19 14:57:27 +0800

    富文本编辑器

---
+++ src/views/richText/editor.vue
@@ -0,0 +1,36 @@
+<template>
+  <quill-editor 
+    v-model="content"
+    ref="myQuillEditor"
+    :options="editorOption"
+    >
+  </quill-editor>
+</template>
+<script>
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+
+import { quillEditor } from "vue-quill-editor";
+
+export default {
+  components: {
+    quillEditor
+  },
+  data() {
+    return {
+      content: "hello world",
+      editorOption: {}
+    };
+  }
+};
+</script>
+
+<style>
+.quill-editor {
+  background-color: #fff;
+}
+.ql-container {
+  height: 200px;
+}
+</style>
